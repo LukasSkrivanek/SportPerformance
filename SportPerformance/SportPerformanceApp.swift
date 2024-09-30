36367dfd40eae15415e02f8edf1a395cad2e9546
LukasSkrivanek <luky.skrivos@gmail.com>
2024-09-30 12:04:49 +0200

    Initial Commit

---
+++ SportPerformance/SportPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportPerformanceApp.swift
+//  SportPerformance
+//
+//  Created by macbook on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SportPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
